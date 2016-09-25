a011f07524c69998726cf4d38d0d253f946fe76e
vegdahl <vegdahl@up.edu>
2016-09-24 17:44:32 -0700

    Initial commit.

---
+++ app/src/main/java/edu/up/cs301/tictactoe/TTTHumanPlayer1a.java
@@ -0,0 +1,160 @@
+package edu.up.cs301.tictactoe;
+
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.RectF;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import edu.up.cs301.animation.AnimationSurface;
+import edu.up.cs301.animation.Animator;
+import edu.up.cs301.game.GameMainActivity;
+import edu.up.cs301.game.R;
+import edu.up.cs301.game.infoMsg.GameInfo;
+import edu.up.cs301.game.infoMsg.IllegalMoveInfo;
+import edu.up.cs301.game.infoMsg.NotYourTurnInfo;
+
+/**
+ * A GUI that allows a human to play tic-tac-toe. Moves are made by clicking
+ * regions on a canvas
+ *
+ * @author Steven R. Vegdahl
+ * @version September 2016
+ */
+public class TTTHumanPlayer1a extends TTTHumanPlayer implements View.OnTouchListener {
+
+    // the current activity
+    private Activity myActivity;
+
+    // the surface view
+    private TTTSurfaceView surfaceView;
+
+    // the state
+    TTTState state;
+
+    // the ID for the layout to use
+    int layoutId;
+
+    /**
+     * constructor
+     *
+     * @param name
+     * 		the player's name
+     * @param layoutId
+     *      the id of the layout to use
+     */
+    public TTTHumanPlayer1a(String name, int layoutId) {
+        super(name);
+        this.layoutId = layoutId;
+    }
+
+    /**
+     * Callback method, called when player gets a message
+     *
+     * @param info
+     * 		the message
+     */
+    @Override
+    public void receiveInfo(GameInfo info) {
+
+        if (surfaceView == null) return;
+
+        if (info instanceof IllegalMoveInfo || info instanceof NotYourTurnInfo) {
+            // if the move was out of turn or otherwise illegal, flash the screen
+            surfaceView.flash(Color.RED, 50);
+        }
+        else if (!(info instanceof TTTState))
+            // if we do not have a TTTState, ignore
+            return;
+        else {
+            // update our 'state' variable with the new state
+            this.state = (TTTState) info;
+            surfaceView.setState(state);
+            surfaceView.invalidate();
+            Log.i("human player", "receiving");
+        }
+    }
+
+    /**
+     * sets the current player as the activity's GUI
+     */
+    public void setAsGui(GameMainActivity activity) {
+
+        // remember our activitiy
+        myActivity = activity;
+
+        // Load the layout resource for the new configuration
+        activity.setContentView(layoutId);
+
+        // set the surfaceView instance variable
+        surfaceView = (TTTSurfaceView)myActivity.findViewById(R.id.surfaceView);
+        Log.i("set listener","OnTouch");
+        surfaceView.setOnTouchListener(this);
+        if (state != null) {
+            receiveInfo(state);
+        }
+        surfaceView.setState(state);
+    }
+
+    /**
+     * returns the GUI's top view
+     *
+     * @return
+     * 		the GUI's top view
+     */
+    @Override
+    public View getTopView() {
+        return myActivity.findViewById(R.id.top_gui_layout);
+    }
+
+    /**
+     * perform any initialization that needs to be done after the player
+     * knows what their game-position and opponents' names are.
+     */
+    protected void initAfterReady() {
+        myActivity.setTitle("Tic-Tac-Toe: "+allPlayerNames[0]+" vs. "+allPlayerNames[1]);
+    }
+
+    /**
+     * callback method when the screen it touched. We're
+     * looking for a screen touch (which we'll detect on
+     * the "up" movement" onto a tic-tac-tie square
+     *
+     * @param event
+     * 		the motion event that was detected
+     */
+    public boolean onTouch(View v, MotionEvent event) {
+        // ignore if not an "up" event
+        if (event.getAction() != MotionEvent.ACTION_UP) return true;
+        // get the x and y coordinates of the touch-location;
+        // convert them to square coordinates (where both
+        // values are in the range 0..2)
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        Point p = surfaceView.mapPixelToSquare(x, y);
+
+        // if the location did not map to a legal square, flash
+        // the screen; otherwise, create and send an action to
+        // the game
+        if (p == null) {
+            surfaceView.flash(Color.RED, 50);
+        } else {
+            TTTMoveAction action = new TTTMoveAction(this, p.y, p.x);
+            Log.i("onTouch", "Human player sending TTTMA ...");
+            game.sendAction(action);
+            surfaceView.invalidate();
+        }
+
+        // register that we have handled the event
+        return true;
+
+    }
+
+
+}
